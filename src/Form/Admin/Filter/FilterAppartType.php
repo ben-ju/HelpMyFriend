91510ac055a4f4d89d93e5db0a82969db17e667d
CORP\bbencivi <benjamin.bencivinni@capgemini.com>
2022-03-18 14:50:12 +0100

    fix: packages problems

--- src/Form/Admin/Filter/FilterAppartType.php
+++ src/Form/Admin/Filter/FilterAppartType.php
@@ -2,17 +2,14 @@
 
 namespace App\Form\Admin\Filter;
 
-use App\Controller\ListingController;
 use App\Entity\Appart;
+use App\Controller\ListingController;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Security\Core\Security;
-
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class FilterAppartType extends AbstractType
 {
@@ -24,22 +21,14 @@ class FilterAppartType extends AbstractType
     private ParameterBagInterface $params;
 
     /**
-     * @var Security
-     */
-    private Security $security;
-
-
-    /**
      * StudentType constructor.
      * @param EntityManagerInterface $entityManager
      * @param ParameterBagInterface $params
-     * * @param Security $security
      */
-    public function __construct(EntityManagerInterface $entityManager, ParameterBagInterface $params, Security $security)
+    public function __construct(EntityManagerInterface $entityManager, ParameterBagInterface $params)
     {
         $this->entityManager = $entityManager;
         $this->params = $params;
-        $this->security = $security;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
